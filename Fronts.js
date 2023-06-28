5fee0c564f1d763b9d7640442d18269d83760bc6
Khrista K <khristakhwaja@gmail.com>
2023-06-28 14:17:40 -0700

    edited the format

--- Fronts.js
+++ Fronts.js
@@ -1,37 +1,6 @@
 // Fronts
 // Use classes, attributes, and methods in the JavaScript language for all challenges. Use only a single JavaScript file for this assignment. All examples are done in order, starting with a new instance of the SLL class.
 
-
-// class Node {
-//     constructor(data) {
-//         this.data = data;
-//         this.next = null;              
-//     }
-// }
-// class LinkedList {
-//     constructor() {
-//         this.head = null;
-//     }
-//     addFront(val) {
-//         // Creating a new node object with the value provided
-//         let new_node = new Node(val);
-//         // Checking to see if the current list does not have a head node (if the list is empty)
-//         // If the list is empty, place the new node as the head 
-//         if(!this.head) {
-//             this.head = new_node;
-//             return this;
-//         }
-//         // If the list is not empty, assign the head to be the next node to the new node (Blue Arrow in picture above)
-//         new_node.next = this.head;
-//         // Then finally assign the new_node to be the new head of the list (Red Arrow in picture above)
-//         this.head = new_node;
-//         return this;
-//     }
-// }
-
-// Add Front
-// Write a method that accepts a value and create a new node, assign it to the list head, and return a pointer to the new head node.
-
 class Node {
     constructor(value) {
         this.data = value;
@@ -42,7 +11,8 @@ class SLL {
     constructor() {
         this.head = null;
 }
-
+// Add Front
+// Write a method that accepts a value and create a new node, assign it to the list head, and return a pointer to the new head node.
     addFront(value) {
         var newNode = new Node(value);
         newNode.next = this.head;
@@ -52,7 +22,6 @@ class SLL {
 
 // Remove Front
 // Write a method to remove the head node and return the new list head node. If the list is empty, return null.
-
     removeFront() {
         if (this.head == null) {
             return this.head;
@@ -76,7 +45,6 @@ class SLL {
 }
 
 var SLL1 = new SLL();
-
 console.log(SLL1.front())
 SLL1.addFront(18)
 console.log(SLL1.front())
